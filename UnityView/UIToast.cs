853dd6b89bd5dadf8cd88310d0208cb1634ca72d
ShadowBlade <1021669533@qq.com>
2016-03-13 13:36:02 +0800

    2016/3/3

---
+++ UnityView/UIToast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityView
+{
+    public class UIToast : UIView
+    {
+        private UIToast() { }
+        public static void MakeToast()
+        {
+            
+        }
+    }
+
+    public class UIToastController : MonoBehaviour
+    {
+        public float LifeTime;
+        void Upadte()
+        {
+            
+        }
+    }
+}
